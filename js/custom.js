3ba61bc9f31d1c28045c2071fe090e929b92bf7a
arturo1984 <abuelo40@gmail.com>
2018-05-16 16:45:21 -0500

    actualizacion del git

--- js/custom.js
+++ js/custom.js
@@ -68,6 +68,9 @@ $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	var sideNavScrollbar = document.querySelector('.custom-scrollbar');
 	Ps.initialize(sideNavScrollbar);
+	
+	
+	
 });
 
 
